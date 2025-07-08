60fb88670b0950ffbe0fbb1e92733b1ae69f72c8
CSEHema <hemasiri1405@gmail.com>
2025-07-08 12:26:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,5 @@ def aware():
     return render_template('aware.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Use Render-provided PORT or default to 5000
+    app.run(host='0.0.0.0', port=port)
